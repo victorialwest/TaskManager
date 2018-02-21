3c544cd4a879cc8b6c5ee4f67989df396877266e
Victoria West <victoriawest811@gmail.com>
2018-02-21 13:54:28 -0500

    Added try catch and switch case statements

--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MainMenu {
@@ -8,11 +10,43 @@ public class MainMenu {
 
     protected void UserPrompt() {
 
+        ArrayList<String> title = new ArrayList<String>();
+
         System.out.println("What would you like to do with the Task Manager?");
-        System.out.println("1. Create task \n2. Edit task \n3. Complete Task \n4. View all tasks \n5. View completed tasks" );
+        System.out.println("1. Create task \n2. Edit task \n3. Complete Task \n4. View all tasks \n5. View completed tasks");
         System.out.println("6. View incomplete tasks \n7. Exit");
         input = new Scanner(System.in);
         int selection = input.nextInt();
-    }
 
+        try {
+            switch (selection) {
+
+                case 1:
+                    //create task
+                case 2:
+                    //edit task
+                case 3:
+                    //complete task
+                case 4:
+                    //view all tasks
+                case 5:
+                    //view completed tasks
+                case 6:
+                    //view incomplete tasks
+                case 7:
+                    //exit
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Please enter a number between 1 and 7");
+                    UserPrompt();
+            }
+
+        } catch (InputMismatchException ime) {
+            input.nextLine();
+            System.out.println("Please enter a number between 1 and 7.");
+            UserPrompt();
+
+        }
+    }
 }
